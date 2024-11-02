54018f5e7a3f7a159cef055e14f09321c7e522e4
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-02 21:23:32 +0300

    feat | Implement LogDataSourceErrorAspect

--- src/main/java/andrew/samardak/spring_aop/aspect/LogDataSourceErrorAspect.java
+++ src/main/java/andrew/samardak/spring_aop/aspect/LogDataSourceErrorAspect.java
@@ -1,9 +1,35 @@
 package andrew.samardak.spring_aop.aspect;
 
+import andrew.samardak.spring_aop.entity.DataSourceErrorLog;
+import andrew.samardak.spring_aop.service.LogDataSourceErrorService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LogDataSourceErrorAspect {
+
+    LogDataSourceErrorService service;
+
+    @AfterThrowing(
+            pointcut = "@annotation(LogDataSourceError)",
+            throwing = "ex"
+    )
+    public void afterThrowing(JoinPoint joinPoint, Exception ex) {
+        DataSourceErrorLog entity = DataSourceErrorLog.builder()
+                .stackTrace(Arrays.toString(ex.getStackTrace()))
+                .message(ex.getMessage())
+                .methodSignature(joinPoint.getSignature().toLongString()).build();
+
+        service.getRepository().save(entity);
+    }
 }
