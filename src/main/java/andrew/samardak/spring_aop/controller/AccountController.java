bdc93925a8f2291e0cddb214944f34c942bbb4ef
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-02 21:24:21 +0300

    feat | Add necessary annotations to project controllers

--- src/main/java/andrew/samardak/spring_aop/controller/AccountController.java
+++ src/main/java/andrew/samardak/spring_aop/controller/AccountController.java
@@ -1,7 +1,17 @@
 package andrew.samardak.spring_aop.controller;
 
+import andrew.samardak.spring_aop.service.AccountService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/accounts")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AccountController {
+
+    AccountService service;
 }
--- src/main/java/andrew/samardak/spring_aop/controller/TransactionController.java
+++ src/main/java/andrew/samardak/spring_aop/controller/TransactionController.java
@@ -1,7 +1,17 @@
 package andrew.samardak.spring_aop.controller;
 
+import andrew.samardak.spring_aop.service.TransactionService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/transactions")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class TransactionController {
+
+    TransactionService service;
 }
