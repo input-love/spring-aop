29afc2c7c605be15206f564284ac07180c5854d9
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-03 19:10:04 +0300

    feat | Add @UpdateTimestamp to transactionTime in Transaction entity

--- src/main/java/andrew/samardak/spring_aop/entity/Transaction.java
+++ src/main/java/andrew/samardak/spring_aop/entity/Transaction.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -27,5 +28,6 @@ public class Transaction {
 
     private BigDecimal amount;
 
+    @UpdateTimestamp
     private LocalDateTime transactionTime;
 }
