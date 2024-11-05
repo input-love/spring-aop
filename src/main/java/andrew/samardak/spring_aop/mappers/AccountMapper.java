f7a4f18e15f46c168df2679169e5d8f5603570bf
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-05 17:01:52 +0300

    refactor | Configure mappers as Spring beans with generics

--- src/main/java/andrew/samardak/spring_aop/mappers/AccountMapper.java
+++ src/main/java/andrew/samardak/spring_aop/mappers/AccountMapper.java
@@ -4,14 +4,7 @@ import andrew.samardak.spring_aop.dto.request.AccountRequestDto;
 import andrew.samardak.spring_aop.dto.response.AccountResponseDto;
 import andrew.samardak.spring_aop.entity.Account;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
-public interface AccountMapper {
-
-    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
-
-    Account toEntity(AccountRequestDto dto);
-
-    AccountResponseDto toDto(Account entity);
+@Mapper(componentModel = "spring")
+public interface AccountMapper extends BaseMapper<Account, AccountRequestDto, AccountResponseDto> {
 }
--- src/main/java/andrew/samardak/spring_aop/mappers/TransactionMapper.java
+++ src/main/java/andrew/samardak/spring_aop/mappers/TransactionMapper.java
@@ -4,14 +4,7 @@ import andrew.samardak.spring_aop.dto.request.TransactionRequestDto;
 import andrew.samardak.spring_aop.dto.response.TransactionResponseDto;
 import andrew.samardak.spring_aop.entity.Transaction;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
-public interface TransactionMapper {
-
-    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
-
-    Transaction toEntity(TransactionRequestDto dto);
-
-    TransactionResponseDto toDto(Transaction entity);
+@Mapper(componentModel = "spring")
+public interface TransactionMapper extends BaseMapper<Transaction, TransactionRequestDto, TransactionResponseDto> {
 }
