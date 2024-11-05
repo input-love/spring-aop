9f5aadf2c5f795d5b17f0f5959802b049701042d
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-05 16:59:05 +0300

    refactor | Remove unnecessary annotations

--- src/main/java/andrew/samardak/spring_aop/dto/request/TransactionRequestDto.java
+++ src/main/java/andrew/samardak/spring_aop/dto/request/TransactionRequestDto.java
@@ -1,19 +1,11 @@
 package andrew.samardak.spring_aop.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class TransactionRequestDto {
 
-    @NotNull
     private BigDecimal amount;
 }
--- src/main/java/andrew/samardak/spring_aop/dto/response/AccountResponseDto.java
+++ src/main/java/andrew/samardak/spring_aop/dto/response/AccountResponseDto.java
@@ -1,17 +1,11 @@
 package andrew.samardak.spring_aop.dto.response;
 
 import andrew.samardak.spring_aop.utils.enums.AccountType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class AccountResponseDto {
 
     private Long id;
